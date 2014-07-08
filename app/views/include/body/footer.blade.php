9c218f7fdf656c647e50a24f2b537bbcf07b0411
Christopher Pitt <cgpitt@gmail.com>
2014-07-08 09:17:03 +0200

    Last few changes.

---
+++ app/views/include/body/footer.blade.php
@@ -0,0 +1,10 @@
+<!--<a href="/">English</a> •-->
+<!--<a href="/ru">Russian</a>-->
+<!--<br />-->
+<a href="/">Introduction</a> •
+<a href="https://twitter.com/assertchris">Twitter</a> •
+<a href="https://medium.com/rebuilding-mootools">Medium</a> •
+<a href="https://leanpub.com/rebuildingmootools">Book</a> •
+<a href="https://medium.com/feed/rebuilding-mootools">Feed</a>
+<br />
+Design by <a href="https://twitter.com/stidges">Stidges</a> • Content by <a href="https://twitter.com/assertchris">Christopher Pitt</a>
